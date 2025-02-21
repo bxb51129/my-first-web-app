e20d631d2557554b75b7f0be068e71a51ee62956
bxb1123 <bxb51129@gmail.com>
2025-02-20 23:47:46 -0600

    Fix Vercel 404 issue

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, Vite + React!</h1>
+      <p>如果你看到这句话，说明 React 已经正确渲染</p>
+    </div>
+  );
+}
+
+export default App;
